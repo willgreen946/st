7e310673203588be51b0080fe9d3052f9f790ecc
Will Green <72352225+willgreen946@users.noreply.github.com>
2024-05-05 18:46:53 +0100

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -15,20 +15,22 @@ typedef struct {
  */
 #if defined(__FreeBSD__)
 static char *fonts[] = {
-  "Spleen 12x24:pixelsize=24:antialias=true:autohint=true",
-  "gallant 12x22:pixelsize=24:antialias=true:autohint=true",
+	"-misc-fixed-medium-r-normal--15-140-75-75-c-90-koi8-r",
   "terminus:pixelsize=24:antialias=true:autohint=true",
-  "Perfect DOS VGA 437:pixelsize=16:antialias=true:autohint=true",
+  "terminus:bold:pixelsize=24:antialias=true:autohint=true",
+  "Spleen 12x24:pixelsize=24:antialias=true:autohint=true",
+  "gallant 12x22:pixelsize=22:antialias=true:autohint=true",
   "IBM 3270:pixelsize=24:antialias=true:autohint=true",
 };
 #endif /* __FreeBSD__ */
 
 #if defined(__linux__) || defined(__NetBSD__) || defined(__OpenBSD__)
 static char *fonts[] = {
-  "spleen:pixelsize=24:antialias=true:autohint=true",
-  "gallant 12x22:pixelsize=24:antialias=true:autohint=true",
+	"-misc-fixed-medium-r-normal--15-140-75-75-c-90-koi8-r",
   "terminus:pixelsize=24:antialias=true:autohint=true",
-  "Perfect DOS VGA 437:pixelsize=16:antialias=true:autohint=true",
+  "terminus:bold:pixelsize=24:antialias=true:autohint=true",
+  "spleen:pixelsize=24:antialias=true:autohint=true",
+  "gallant 12x22:pixelsize=22:antialias=true:autohint=true",
   "IBM 3270:pixelsize=24:antialias=true:autohint=true",
 };
 #endif /* __linux__ & __NetBSD__ & __OpenBSD__ */
